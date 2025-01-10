5f6099cd17d5ee8f9086bb449f5242ce4a8ade77
Feldwor <BoQsc@users.noreply.github.com>
2025-01-10 23:42:12 +0200

    Update UI_Container.py

--- UI_Container.py
+++ UI_Container.py
@@ -5,7 +5,6 @@ canvas = tkinter.Canvas(window, bg="black")
 canvas.pack()
 
 container = canvas.create_rectangle(50, 50, 300, 100, fill="lightgray")
-
 item1 = canvas.create_rectangle(70, 70, 90, 90, fill="blue")
 item2 = canvas.create_text(125, 100, text="Item2", font=("Arial", 14))
 items = [item1, item2]
