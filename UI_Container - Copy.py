787f2f2980fc8b1cec18b5fbe3d9069929398c50
Feldwor <BoQsc@users.noreply.github.com>
2025-01-11 09:34:09 +0200

    Create UI_Container - Copy.py

---
+++ UI_Container - Copy.py
@@ -0,0 +1,23 @@
+import tkinter
+window = tkinter.Tk()
+
+canvas = tkinter.Canvas(window, bg="black")
+canvas.pack()
+
+container = canvas.create_rectangle(50, 50, 300, 100, fill="lightgray")
+item1 = canvas.create_rectangle(70, 70, 90, 90, fill="blue")
+item2 = canvas.create_text(125, 100, text="Item2", font=("Arial", 14))
+items = [item1, item2]
+
+def move_container(dx, dy):
+    canvas.move(container, dx, dy)
+    for item in items:
+        canvas.move(item, dx, dy)
+
+def resize_container(): # Container gets smaller From the top. Items get smaller from the top as well.
+    pass
+
+button = tkinter.Button(window, text="Move", command=lambda: move_container(10, 10))
+button.pack()
+
+window.mainloop()
