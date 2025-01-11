d99935b2f868d42ab82e9cc33dfa196e5916c5b9
Feldwor <BoQsc@users.noreply.github.com>
2025-01-11 10:02:58 +0200

    Update UI_Container - Copy.py

--- UI_Container - Copy.py
+++ UI_Container - Copy.py
@@ -8,13 +8,11 @@ canvas.create_rectangle(50, 50, 300, 100, fill="lightgray", tags="container")
 canvas.create_rectangle(70, 70, 90, 90, fill="blue", tags="container_item")
 canvas.create_text(125, 100, text="Item2", font=("Arial", 14), tags="container_item")
 
-
 def move_container(container, dx, dy):
     canvas.move(canvas.find_withtag(container), dx, dy)
     for item in canvas.find_withtag(container + "_item"):
         print(canvas.gettags(item))
         canvas.move(item, dx, dy)
-
 
 def resize_container(): # Container gets smaller From the top. Items get smaller from the top as well.
     pass
