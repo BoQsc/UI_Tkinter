ff8275af6a1912c6ccef3186f8679cb5c43c52cd
Feldwor <BoQsc@users.noreply.github.com>
2025-01-11 10:02:28 +0200

    Update UI_Container - Copy.py

--- UI_Container - Copy.py
+++ UI_Container - Copy.py
@@ -4,20 +4,22 @@ window = tkinter.Tk()
 canvas = tkinter.Canvas(window, bg="black")
 canvas.pack()
 
-container = canvas.create_rectangle(50, 50, 300, 100, fill="lightgray")
-item1 = canvas.create_rectangle(70, 70, 90, 90, fill="blue")
-item2 = canvas.create_text(125, 100, text="Item2", font=("Arial", 14))
-items = [item1, item2]
+canvas.create_rectangle(50, 50, 300, 100, fill="lightgray", tags="container")
+canvas.create_rectangle(70, 70, 90, 90, fill="blue", tags="container_item")
+canvas.create_text(125, 100, text="Item2", font=("Arial", 14), tags="container_item")
 
-def move_container(dx, dy):
-    canvas.move(container, dx, dy)
-    for item in items:
+
+def move_container(container, dx, dy):
+    canvas.move(canvas.find_withtag(container), dx, dy)
+    for item in canvas.find_withtag(container + "_item"):
+        print(canvas.gettags(item))
         canvas.move(item, dx, dy)
+
 
 def resize_container(): # Container gets smaller From the top. Items get smaller from the top as well.
     pass
 
-button = tkinter.Button(window, text="Move", command=lambda: move_container(10, 10))
+button = tkinter.Button(window, text="Move", command=lambda: move_container("container", 10, 10))
 button.pack()
 
 window.mainloop()
