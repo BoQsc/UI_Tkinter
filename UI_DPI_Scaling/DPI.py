33098f3f6d5692f5e19599514029feeee17abfd4
Feldwor <BoQsc@users.noreply.github.com>
2025-01-28 10:31:44 +0200

    Update DPI.py

--- UI_DPI_Scaling/DPI.py
+++ UI_DPI_Scaling/DPI.py
@@ -9,4 +9,6 @@ print("screen_height: ", window.winfo_screenheight())
 print("window_width: ", window.winfo_width())
 print("window_height: ", window.winfo_height())
 print("dpi (pixels per inch): ", window.winfo_fpixels('1i'))  # '1i' is the Tkinter unit for 1 inch
+print("winfo_pixels: ", window.winfo_pixels("1i"))
+
 window.mainloop()
