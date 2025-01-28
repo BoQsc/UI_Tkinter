f9c0e27a5dfbde7cd868bcc23f8135d18e2c85c0
Feldwor <BoQsc@users.noreply.github.com>
2025-01-28 16:43:54 +0200

    update

--- UI_DPI_Scaling/DPI.py
+++ UI_DPI_Scaling/DPI.py
@@ -9,6 +9,7 @@ def on_resize(event):
     if event.widget == window:
         print("screen_width: ", window.winfo_screenwidth())
         print("screen_height: ", window.winfo_screenheight())
+        
         print("dpi (pixels per inch): ", window.winfo_fpixels('1i'))  # '1i' is the Tkinter unit for 1 inch
         print("winfo_pixels: ", window.winfo_pixels("1i"))
         print("tk_scaling_factor:", window.tk.call('tk', 'scaling'))   # Tk's internal scaling factor
