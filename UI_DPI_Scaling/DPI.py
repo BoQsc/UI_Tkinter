bf427554d25eb8672cbcad1bd9f0af73b2c0144c
Feldwor <BoQsc@users.noreply.github.com>
2025-01-28 13:36:56 +0200

    Update DPI.py

--- UI_DPI_Scaling/DPI.py
+++ UI_DPI_Scaling/DPI.py
@@ -4,11 +4,17 @@ window = tkinter.Tk()
 canvas = tkinter.Canvas(window, bg="blue")
 canvas.pack()
 
+
+
 print("screen_width: ", window.winfo_screenwidth())
 print("screen_height: ", window.winfo_screenheight())
-print("window_width: ", window.winfo_width())
-print("window_height: ", window.winfo_height())
 print("dpi (pixels per inch): ", window.winfo_fpixels('1i'))  # '1i' is the Tkinter unit for 1 inch
 print("winfo_pixels: ", window.winfo_pixels("1i"))
+print("tk_scaling_factor:", window.tk.call('tk', 'scaling'))   # Tk's internal scaling factor
+
+window.update()
+print("window_width: ", window.winfo_width())
+print("window_height: ", window.winfo_height())
+
 
 window.mainloop()
